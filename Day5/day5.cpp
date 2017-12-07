034b398e979f3150f97687d9db07b2c2c1d2dabd
Marcos Marín <marcosmarin@gmail.com>
2017-12-06 20:29:41 -0800

    Day 5

--- Day5/day5.cpp
+++ Day5/day5.cpp
@@ -1,7 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
+
+static std::vector<int> ReadInput()
+{
+	std::vector<int> result;
+
+	const char* filename = "input.txt";
+
+	FILE* fp = nullptr;
+	if (fopen_s(&fp, filename, "r") != 0)
+	{
+		printf("Error opening input file '%s'.", filename);
+		return result;
+	}
+	
+	char buffer[1024];
+	while (fgets(buffer, _countof(buffer), fp) != nullptr)
+	{
+		int n = atoi(buffer);
+		result.push_back(n);
+	}
+
+	fclose(fp);
+
+	return result;
+}
 
 int main(int argc, char** argv)
 {
+	std::vector<int> instructions;
+	int ip = 0;
+	int steps = 0;
+
+	if (argc == 1)
+	{
+		instructions = ReadInput();
+	}
+	else
+	{
+		instructions = std::vector<int>({ 0, 3, 0, 1, -3 });
+	}
+
+	const int size = instructions.size();
+	while (ip >= 0 && ip < size)
+	{
+		int& instruction = instructions[ip];
+		ip += instruction++;
+		steps++;
+	}
+
+	printf("Result: %d\n", steps);
+
 	return 0;
 }
